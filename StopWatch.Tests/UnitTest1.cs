03dc7f9118888050ee05b3056c075184198056f8
Eric De Carufel <eric.decarufel@gmail.com>
2021-05-20 13:07:54 -0400

    Initial Commit

---
+++ StopWatch.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Configuration;
+using System.Threading;
+using NSubstitute;
+using Xunit;
+
+namespace StopWatch.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Chrono_running_for_10_second()
+        {
+        }
+    }
+}
---
+++ StopWatch/IChrono.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public interface IChrono
+    {
+        void StartStop();
+        void LapReset();
+        TimeSpan Elapsed { get; }
+    }
+}
